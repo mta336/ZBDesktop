89abf4fb8c236c8048595e3474e7adff288ae4a2
michael.amoruso <michael.amoruso@SUNHILLOHEPC6.SunhilloCorp.local>
2017-04-19 11:58:16 -0400

    Initial Check In

---
+++ ZombieBird-desktop/src/com/litgames/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.litgames.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.litgames.game.ZBGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZBGame(), config);
+	}
+}
